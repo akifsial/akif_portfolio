e9791fb59ca70264386857f5b3a332f257858ac7
akifsial <contact.akifsial@gmail.com>
2026-06-20 04:43:26 +0500

    Remove basePath

--- next.config.ts
+++ next.config.ts
@@ -1,21 +1,22 @@
 import type { NextConfig } from "next";
 
-const basePath = process.env.NODE_ENV === "production" ? "/Resume-Nextjs" : "";
+// const basePath = process.env.NODE_ENV === "production" ? "/Resume-Nextjs" : "";
 
 const nextConfig: NextConfig = {
   output: "export",
-  basePath,
-  assetPrefix: basePath,
+  // basePath,
+  // assetPrefix: basePath,
   images: {
     unoptimized: true,
   },
   trailingSlash: true,
+
   // eslint: {
   //   ignoreDuringBuilds: true,
   // },
-  env: {
-    NEXT_PUBLIC_BASE_PATH: basePath,
-  },
+  // env: {
+  //   NEXT_PUBLIC_BASE_PATH: basePath,
+  // },
 };
 
 export default nextConfig;
